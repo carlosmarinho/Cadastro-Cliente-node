4646c0a02ef63505b88926f362b94469deb8fadd
rails <carlos.marinho@umbarato.com.br>
2016-10-21 21:58:27 -0200

    adicionado funções de cadastro, listar no controllerr do cliente

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
+	 'new': function (req, res) {
+      res.view();
+    },
+    index: function (req, res) {
+      Client.find(function foundClient (err, clients) {
+        if(err) return res.redirect('/client/new');
+        res.view({
+          clients: clients
+        });
+      });
+    },
+    create: function(req, res) {
+      Client.create(req.params.all(), function clientCreated(err, client) {
+        if (err) return res.redirect('/client/new');
+        res.redirect('/client/show/' + client.id); 
+      });
+    }
 	
 };
 
